71df8d2ed646f184e44d6cf2037bfb8f10396425
RonanD10 <69147805+RonanD10@users.noreply.github.com>
2024-10-15 21:23:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from langchain_helper import create_vector_db, get_qa_chain
 
 st.title("Medical Q & A")
 
+st.text("For illustration, trained on heart and lung medical data.") 
+st.text("Example questions: \n 
+- 'What are the symptoms of pneomonia?'\n
+- 'How common is asthma?'")
+
 question = st.text_input("Question: ")
 
 if question:
